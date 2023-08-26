cd012a6c5a30a99ed47edcec9a38ae7e2bc97c93
Nicolas Mayoral <n.mayoral@alumno.um.edu.ar>
2023-08-26 11:45:49 -0300

    #3 Screen View and Component structure created in the project

--- PlatformUM/App.js
+++ PlatformUM/App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Platform } from 'react-native';
+import { Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Platform: {Platform.OS}</Text>
+      <Text>Version: {Platform.Version}</Text>
+      <Text style={styles.header}>Hello World!</Text>
+
+      <Button
+        title="SELECT"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +25,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    fontSize: 24, // Tamaño de la fuente en puntos
+    fontWeight: 'bold', // Fuente en negrita
+    textAlign: 'center', // Alineación central del texto
   },
 });
